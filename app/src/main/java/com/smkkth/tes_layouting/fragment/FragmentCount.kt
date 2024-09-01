798048fb3b84b75c7f831dc34bcac1597136f11a
abdyllaann <adhiyasaabdylla@gmail.com>
2024-09-01 11:40:36 +0700

    main

--- app/src/main/java/com/smkkth/tes_layouting/fragment/FragmentCount.kt
+++ app/src/main/java/com/smkkth/tes_layouting/fragment/FragmentCount.kt
@@ -24,9 +24,6 @@ class FragmentCount : Fragment() {
         // Inflate the layout for this fragment
         binding =  FragmentCountBinding.inflate(inflater, container, false)
         val view = binding.root
-
-
-
         return view
     }
 
@@ -40,68 +37,111 @@ class FragmentCount : Fragment() {
             parentFragmentManager.beginTransaction().replace(R.id.Fragment_Container, FragmentInput()).commit()
         }
         binding.btnreset.setOnClickListener {
-            val builder = AlertDialog.Builder(requireContext())
-            builder.setTitle("Peringatan!!")
-            builder.setMessage("Ingin mereset data?")
-            builder.setPositiveButton("Nama dan Skor"){
-                dialog, which->
-                val editor = sharePref?.edit()
-                editor?.remove("KeyPlayer1")
-                editor?.remove("KeyPlayer2")
-                editor?.apply()
-                onResume()
-                player1skor = 0
-                binding.count1.text = player1skor.toString()
-                player2skor = 0
-                binding.count2.text = player2skor.toString()
-            }
-            builder.setNegativeButton("Skor"){
-                dialog, which ->
-                player1skor = 0
-                binding.count1.text = player1skor.toString()
-                player2skor = 0
-                binding.count2.text = player2skor.toString()
-            }
-            builder.setNeutralButton("Batal"){
-                dialog, which ->
-                dialog.dismiss()
-            }
-            builder.show()
-
+            resetDialog()
         }
 
+        binding.count1.setOnClickListener {
+            player1skor++
+            updateskor()
+            checkwinner()
+        }
+        binding.count2.setOnClickListener {
+            player2skor++
+            updateskor()
+            checkwinner()
+        }
 
     }
 
     override fun onResume() {
         super.onResume()
+        loadNamaPlayer()
+//        if (player1skor == 21 || player2skor == 21){
+//            val winner = when {
+//                player1skor > player2skor -> "Player 1"
+//                player2skor > player1skor -> "Player 2"
+//                else -> "No one" // Case when both have the same score, but the score is >= 21
+//            }
+//            val builder = AlertDialog.Builder(requireContext())
+//            builder.setTitle("Pertandingan selesai")
+//            builder.setMessage(" $winner Menang. Ingin memulai ulang dari awal?")
+//            builder.setPositiveButton("ya"){
+//                    dialog, which ->
+//                dialog.dismiss()
+//            }
+//            builder.show()
+//        } else{
+//            binding.count1.setOnClickListener {
+//                player1skor++
+//                binding.count1.text = player1skor.toString()
+//            }
+//            binding.count2.setOnClickListener {
+//                player2skor++
+//                binding.count2.text = player2skor.toString()
+//
+//            }
+//        }
+//
+        }
+
+
+    private fun loadNamaPlayer(){
         sharePref = activity?.getSharedPreferences("player", Context.MODE_PRIVATE)
         val nama1 = sharePref?.getString("KeyPlayer1", "Player 1")
         val nama2 = sharePref?.getString("KeyPlayer2", "Player 2")
 
         binding.player1.text = nama1
         binding.player2.text = nama2
+    }
+    private fun resetDialog(){
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Peringatan!!")
+        builder.setMessage("Ingin mereset data?")
+        builder.setPositiveButton("Nama dan Skor"){
+                dialog, which->
+            sharePref?.edit()?.clear()?.apply()
+            loadNamaPlayer()
+            player1skor = 0
+            player2skor = 0
+            updateskor()
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("Skor"){
+                dialog, which ->
+            player1skor = 0
+            player2skor = 0
+            updateskor()
+            dialog.dismiss()
+        }
+        builder.setNeutralButton("Batal"){
+                dialog, which ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
 
+    private fun updateskor(){
+        binding.count1.text = player1skor.toString()
+        binding.count2.text = player2skor.toString()
+    }
 
-        if (player1skor == 21 || player2skor == 21){
+    private fun checkwinner(){
+        if (player1skor >=21 || player2skor >=21 ){
             val builder = AlertDialog.Builder(requireContext())
             builder.setTitle("Pertandingan selesai")
             builder.setMessage("Ingin memulai ulang dari awal?")
             builder.setPositiveButton("ya"){
                     dialog, which ->
+                player1skor = 0
+                player2skor = 0
+                updateskor()
                 dialog.dismiss()
             }
-        } else{
-            binding.count1.setOnClickListener {
-                player1skor++
-                binding.count1.text = player1skor.toString()
+            builder.setNeutralButton("Tidak"){
+                dialog, which ->
+                dialog.dismiss()
             }
-            binding.count2.setOnClickListener {
-                player2skor++
-                binding.count2.text = player2skor.toString()
-
-            }
+            builder.show()
         }
-
-        }
+    }
 }
